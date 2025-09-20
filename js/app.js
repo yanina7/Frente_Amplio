a3bb2811c7c1be0854df8fa2f2c36744fca3b71c
yanina7 <yaninamercado4@gmail.com>
2025-09-20 13:34:58 -0500

    Update app.js 8

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  $('#lg').toggle(3000);
-  $('#title-sh').toggle(3500);
+  $('#lg').toggle(2000);
+  $('#title-sh').toggle(2500);
   setTimeout(function() {
     window.location.href = 'views/register.html';
-  }, 4000);
+  }, 3000);
 });
